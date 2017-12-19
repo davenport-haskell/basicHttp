00a00b41c95ea444358ff04b1be472cffcbd0d9d
Christopher Davenport <Chris@ChristopherDavenport.tech>
2017-12-18 23:09:51 -0500

    Initial Tiny WebServer with WAI

---
+++ app/Main.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Network.Wai
+import Network.HTTP.Types (status200)
+import Network.Wai.Handler.Warp (run)
+
+application _ respond = respond $
+  responseLBS status200 [("Content-Type", "text/plain")] "Hello World"
+
+main = run 8000 application
